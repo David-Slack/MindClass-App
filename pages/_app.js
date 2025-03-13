64a4c0cbd0b75bad20fab21ee9c0719f55fb3a08
Dave Slack <davidkslack@yahoo.co.uk>
2025-03-13 14:05:10 +0000

    Added font and removed all unneeded assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,35 @@
 import "@/styles/globals.css";
+import localFont from 'next/font/local';
+import Head from "next/head";
+
+export const dazzed = localFont({
+  src: [
+    {
+      path: './dazzed/Dazzed-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './dazzed/Dazzed-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    }
+  ],
+})
+
+let mainClass = 'home'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+
+        <main className={`${mainClass} ${dazzed.className}`}>
+          <Component {...pageProps} />
+        </main>
+      </>
+  );
 }
