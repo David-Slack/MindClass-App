d48a7873ef6418df6ef6c0eabc92eafd266e9b65
Dave Slack <davidkslack@yahoo.co.uk>
2025-03-18 14:29:17 +0000

    Card #1: Changed lib folder to helpers

--- context/AuthUserContext.jsx
+++ context/AuthUserContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext} from 'react'
-import useFirebaseAuth from '../lib/useFirebaseAuth';
+import useFirebaseAuth from '@/helpers/useFirebaseAuth';
 
 const authUserContext = createContext({
     authUser: null,
