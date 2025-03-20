a3a6d1957f6b13b6d0fa9990dc04b691f6061e46
Dave Slack <davidkslack@yahoo.co.uk>
2025-03-20 13:26:30 +0000

    Card #4: Added the basic login form

---
+++ components/loginForm/LoginForm.js
@@ -0,0 +1,89 @@
+import { Container, Row, Col, Button, Form, Alert } from 'react-bootstrap';
+import Image from "next/image";
+import styles from "./LoginForm.module.css";
+import people from "../../public/img/people.webp";
+import { useAuth } from '@/helpers/firebase/AuthUserContext';
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+
+export function LoginForm( ){
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState(null);
+    const router = useRouter();
+    const { signInWithEmailAndPassword } = useAuth();
+    const { authUser, loading} = useAuth();
+
+    // If we are already logged in, redirect to the home page
+    useEffect(() => {
+        if (authUser)
+            router.push('/').then();
+    }, [authUser]);
+
+    const onSubmit = event => {
+        setError(null);
+        signInWithEmailAndPassword(email, password)
+            .then(authUser => {
+                router.push('/').then();
+            })
+            .catch(error => {
+                setError(error.message);
+            });
+        event.preventDefault();
+    };
+
+    return(
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col xs lg="3" className={`${styles.loginImageRow} ${styles.loginRow}`}>
+                    <Image
+                        src={people}
+                        alt={`People`}
+                        priority={true}
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                </Col>
+                <Col lg={3} className={`${styles.loginFormRow} ${styles.loginRow}`}>
+                    <Form onSubmit={onSubmit}>
+
+                        {error && <Alert color="danger">{error}</Alert>}
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput0">
+                            <h1>Login</h1>
+                        </Form.Group>
+                        <Form.Group className="form-floating mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Control
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={(event) => setEmail(event.target.value)}
+                                placeholder="name@example.com"
+                            />
+                            <Form.Label>Email address</Form.Label>
+                        </Form.Group>
+
+                        <Form.Group className="form-floating mb-3" controlId="exampleForm.ControlInput2">
+                            <Form.Control
+                                type="password"
+                                name="password"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                                placeholder="password"
+                            />
+                            <Form.Label>Password</Form.Label>
+                        </Form.Group>
+
+
+                        <Form.Group className="form-floating mb-3" controlId="exampleForm.ControlInput3">
+                            <Button variant="primary" type="submit">
+                                Login
+                            </Button>
+                        </Form.Group>
+
+                    </Form>
+                </Col>
+            </Row>
+
+        </Container>
+    );
+}
--- pages/login.js
+++ pages/login.js
@@ -1,82 +1,10 @@
-import {useEffect, useState} from 'react';
-import { useRouter } from 'next/router';
-import { useAuth } from '@/helpers/firebase/AuthUserContext';
 
-import { Container, Row, Col, Button, Form, FormGroup, Alert } from 'react-bootstrap';
+import {LoginForm} from "@/components/loginForm/LoginForm";
 
 export default function Login() {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [error, setError] = useState(null);
-    const router = useRouter();
-    const { signInWithEmailAndPassword } = useAuth();
-    const { authUser, loading} = useAuth();
 
-    // If we are already logged in, redirect to the home page
-    useEffect(() => {
-        if (authUser)
-            router.push('/').then();
-    }, [authUser]);
-
-    const onSubmit = event => {
-        setError(null);
-        signInWithEmailAndPassword(email, password)
-            .then(authUser => {
-                router.push('/').then();
-            })
-            .catch(error => {
-                setError(error.message);
-            });
-        event.preventDefault();
-    };
 
     return (
-        <Container className="text-center" style={{ padding: '40px 0px' }}>
-            <Row>
-                <Col>
-                    <h2>Login</h2>
-                </Col>
-            </Row>
-            <Row style={{ maxWidth: '400px', margin: 'auto' }}>
-                <Col>
-                    <Form onSubmit={onSubmit}>
-                        {error && <Alert color="danger">{error}</Alert>}
-                        <FormGroup>
-                            <Form.Label htmlFor="loginEmail" sm={4}>Email</Form.Label>
-                            <Col sm={8}>
-                                <Form.Control
-                                    type="email"
-                                    value={email}
-                                    onChange={(event) => setEmail(event.target.value)}
-                                    name="email"
-                                    id="loginEmail"
-                                    placeholder="Email"
-                                />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup>
-                            <Form.Label htmlFor="loginPassword" sm={4}>Password</Form.Label>
-                            <Col sm={8}>
-                                <Form.Control
-                                    type="password"
-                                    name="password"
-                                    value={password}
-                                    onChange={(event) => setPassword(event.target.value)}
-                                    id="loginPassword"
-                                    placeholder="Password"
-                                />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup>
-                            <Col>
-                                <Button variant="primary" type="submit">
-                                    Login
-                                </Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+        <LoginForm />
     );
 }
