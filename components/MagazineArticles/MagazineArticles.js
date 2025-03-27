23d69134a258a1310dbec31f36c0d208496b3b5c
Dave Slack <davidkslack@yahoo.co.uk>
2025-03-27 13:42:23 +0000

    Card #8: Added magazine filtering

--- components/MagazineArticles/MagazineArticles.js
+++ components/MagazineArticles/MagazineArticles.js
@@ -1,49 +1,49 @@
 import {Badge, Card, Col, Row} from "react-bootstrap";
 import styles from "./MagazineArticles.module.css";
 import Link from "next/link";
-import {useEffect} from "react";
-
-// TODO: Get an array of tags
+import {useState} from "react";
 
 export function MagazineArticles({ magazineArticles }) {
 
-    useEffect(()=>{
+    const allCategories = ["All", "Tips", "Interviews"]; // Get unique categories
 
-        // Remove active from the filter links
-        // Make the link active
+    const [activeFilter, setActiveFilter] = useState("All");
 
-        // Create a backup of the magazineArticles
-        // Pass in the filter
-        // Find all cards with the filter as a tag
-        // Remove them from magazineArticles
-        // Update the map
-
-    });
+    const filteredMagazineArticles =
+        activeFilter === "All"
+            ? magazineArticles
+            : magazineArticles.filter((article) => article.type.includes(activeFilter));
 
     return (
         <>
             <Row className={styles.filter}>Filter by:</Row>
             <Row className={styles.filter}>
 
-                <Badge className={`${styles.active} ${styles.badge}`} pill>All</Badge>
-                <Badge className={styles.badge} pill>Tags</Badge>
-                <Badge className={styles.badge} pill>Interviews</Badge>
+                {allCategories.map((category) => (
+                    <Badge
+                        key={category}
+                        onClick={() => setActiveFilter(category)}
+                        className={`${styles.active} 
+                        ${styles.badge}`}
+                        pill
+                    >{category}</Badge>
+                ))}
             </Row>
 
             <Row>
 
-                { magazineArticles.map((item, count) => (
+                { filteredMagazineArticles.map((article, count) => (
 
-                    <Col className={`mx-auto ${styles.col}`} key={item.id} lg={count > 1 ? "3" : "6"}>
-                        <Card className={`mx-auto ${styles.card}`} bg={item.colour}>
+                    <Col className={`mx-auto ${styles.col}`} key={article.id} lg={count > 1 ? "3" : "6"}>
+                        <Card className={`mx-auto ${styles.card}`} bg={article.colour}>
                             <Card.Body  className={styles.body}>
-                                <Card.Title className={styles.title}>{item.title}</Card.Title>
+                                <Card.Title className={styles.title}>{article.title}</Card.Title>
                                 <Card.Text>
-                                    <Badge className={styles.badge} pill>{item.type}</Badge>
+                                    <Badge className={styles.badge} pill>{article.type}</Badge>
                                     <span className={styles.arrow}>&rarr;</span>
                                 </Card.Text>
                             </Card.Body>
-                            <Card.Img src={item.image} className={styles.img} />
+                            <Card.Img src={article.image} className={styles.img} />
                             <Link href="/" className="stretched-link"></Link>
                         </Card>
                     </Col>
