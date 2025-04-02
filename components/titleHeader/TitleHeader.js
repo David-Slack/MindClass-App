92e38de8954b4ca95f680c17fa908571148036af
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-02 15:17:26 +0100

    Card #14: Added confetti on click of the smiley face

--- components/titleHeader/TitleHeader.js
+++ components/titleHeader/TitleHeader.js
@@ -1,15 +1,48 @@
-import {Container, Row} from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import styles from "./TitleHeader.module.css";
+import ConfettiExplosion from 'react-confetti-explosion';
+import { useState, useEffect } from "react";
 
-export function TitleHeader({title, subtitle}){
-    return(
+export function TitleHeader({ title, subtitle }) {
+    const [isExploding, setIsExploding] = useState(false);
+
+    useEffect(() => {
+        return () => {
+            // Cleanup: Remove the portal's DOM node
+            const confettiElements = document.querySelectorAll('.confetti-explosion-wrapper');
+            confettiElements.forEach(element => {
+                element.remove();
+            });
+        };
+    }, []); // Run only on unmount
+
+    const handleSmileyClick = () => {
+        setIsExploding(true);
+    };
+
+    const handleConfettiComplete = () => {
+        setIsExploding(false);
+    };
+
+    return (
         <Container fluid className={styles.titleHeader}>
             <Row>
-                <h1>{title}
-                    <div className={styles.smiley} ></div>
+                <h1>
+                    {title}
+                    <div className={styles.smiley} onClick={handleSmileyClick}>
+                        {isExploding && (
+                            <ConfettiExplosion
+                                force={0.4}
+                                duration={2200}
+                                particleCount={30}
+                                width={400}
+                                onComplete={handleConfettiComplete}
+                            />
+                        )}
+                    </div>
                 </h1>
                 <p>{subtitle}</p>
             </Row>
         </Container>
-    )
+    );
 }
