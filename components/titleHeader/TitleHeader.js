718907316a48c516f58517be8691997397a760c0
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-02 13:30:24 +0100

    Card #14: Added smiley in the correct place

--- components/titleHeader/TitleHeader.js
+++ components/titleHeader/TitleHeader.js
@@ -5,7 +5,9 @@ export function TitleHeader({title, subtitle}){
     return(
         <Container fluid className={styles.titleHeader}>
             <Row>
-                <h1>{title}</h1>
+                <h1>{title}
+                    <div className={styles.smiley} ></div>
+                </h1>
                 <p>{subtitle}</p>
             </Row>
         </Container>
