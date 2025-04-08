fb3701457b6473d18ff3d3804b761373d09ad47a
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-08 16:55:39 +0100

    Card #7: Removed the header from the course page

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import {getItem} from "@/helpers/firebase/getItem";
-import {TitleHeader} from "@/components/titleHeader/TitleHeader";
+// import {TitleHeader} from "@/components/titleHeader/TitleHeader";
 import {Container} from "react-bootstrap";
 import {CourseIFrame} from "@/components/courseIFrame/CourseIFrame";
 
@@ -19,7 +19,7 @@ export default function Course({ article }) {
                 <title>{title}</title>
                 <meta name="description" content={description} />
             </Head>
-            <TitleHeader title={article.title} subtitle={description} />
+            {/*<TitleHeader title={article.title} subtitle={description} />*/}
             <Container fluid>
                 <CourseIFrame course={article} />
             </Container>
