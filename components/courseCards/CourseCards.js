a60b99e894708e1c132b677609203e4bb0a8d79e
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-09 11:35:33 +0100

    Card #7: Added courses styles

--- components/courseCards/CourseCards.js
+++ components/courseCards/CourseCards.js
@@ -18,7 +18,7 @@ export function CourseCards({ collection }) {
                             <Card.Img src={card.image} className={styles.img} />
                             <Card.Body className={styles.body}>
                                 <Card.Title className={styles.title}>{card.title}</Card.Title>
-                                <Badge className={styles.sessions} pill>
+                                <Badge className={styles.lessons} pill>
                                     {
                                         // We do not have lessons in the DB
                                         card?.lessons > 1
@@ -27,7 +27,7 @@ export function CourseCards({ collection }) {
                                     }
                                 </Badge>
                                 <span className={styles.arrow}>&rarr;</span>
-                                <Card.Text>
+                                <Card.Text className={styles.cardText}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-clock" viewBox="0 0 16 16">
                                         <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
                                         <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
