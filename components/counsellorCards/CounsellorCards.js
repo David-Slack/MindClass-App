c279a70ea9394f2250f367dcaee33e16de41dc9e
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-10 17:21:00 +0100

    Card #10: Added counsellors styles

--- components/counsellorCards/CounsellorCards.js
+++ components/counsellorCards/CounsellorCards.js
@@ -1,36 +1,33 @@
-import { Card, CardGroup, Container, Row } from "react-bootstrap";
+import { Card, CardGroup } from "react-bootstrap";
 import styles from "./CounsellorCards.module.css";
 import Link from "next/link";
 import Image from "next/image"; // Import Next.js Image
 
 export function CounsellorCards({ collection }) {
     return (
-        <Container fluid>
-            <Row>
-                <CardGroup>
+        <CardGroup className={styles.cardGroup}>
 
-                    {collection.map((counsellor) => (
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>{counsellor.name}</Card.Title>
-                                <Card.Text>
-                                    {counsellor.homepageText}
-                                </Card.Text>
-                            </Card.Body>
-                            <Image
-                                src={counsellor.image}
-                                alt={counsellor.name}
-                                className={styles.img}
-                                fill={true}
-                                priority={collection.indexOf(counsellor) < 2} // Prioritize the first few
-                            />
-                            <span className={styles.arrow}>&rarr;</span>
-                            <Link href={`/counsellors/${counsellor.id}`} className="stretched-link"></Link>
-                        </Card>
-                    ))}
+            {collection.map((counsellor) => (
+                <Card className={styles.card}>
+                    <Card.Body>
+                        <Card.Title>{counsellor.name}</Card.Title>
+                        <Card.Text>
+                            {counsellor.homepageText}
+                        </Card.Text>
+                    </Card.Body>
+                    <Image
+                        src={counsellor.image}
+                        alt={counsellor.name}
+                        className={styles.img}
+                        width={655}
+                        height={374}
+                        priority={collection.indexOf(counsellor) < 2} // Prioritize the first few
+                    />
+                    <span className={styles.arrow}>&rarr;</span>
+                    <Link href={`/counsellors/${counsellor.id}`} className="stretched-link"></Link>
+                </Card>
+            ))}
 
-                </CardGroup>
-            </Row>
-        </Container>
+        </CardGroup>
     );
 }
