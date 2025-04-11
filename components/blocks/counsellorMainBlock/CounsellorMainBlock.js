9b091ada4828e4813392ac582f6ff289b4173c1c
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-11 15:24:43 +0100

    Card #10: Changed counsellor main block to obj

--- components/blocks/counsellorMainBlock/CounsellorMainBlock.js
+++ components/blocks/counsellorMainBlock/CounsellorMainBlock.js
@@ -1,30 +1,30 @@
 import {Col, Container, Row} from "react-bootstrap";
 import Image from "next/image";
 
-const blockHTML = `
-<h2>Get started with counselling</h2>
-<p>Hello, I'm Carl, the Clinical Lead here at MindClass. I'm also in charge of safeguarding our counselling service to
+// TODO: Store in the DB with CMS to change
+const block = {
+    title: `Get started with counselling`,
+    body: `<p>Hello, I'm Carl, the Clinical Lead here at MindClass. I'm also in charge of safeguarding our counselling service to
 ensure a safe and welcoming experience for all of our clients. We're big advocates of counselling as it provides a
 supportive environment for you to explore your thoughts, feelings, and behaviours with a trained professional.</p>
 <p>Counselling can help you manage stress, anxiety, depression, and other mental health challenges by offering new 
-perspectives and coping strategies.</p>  
-`;
-
-const blockImageSrc = "https://app.mindclass.co.uk/static/media/counsellorImage.5f296e5ee6d9e5398903.png";
-const blockImageAlt = "Carl Turner, MindClass Clinical Lead, CYPMHS BA (Hons), PG dip";
-
+perspectives and coping strategies.</p>`,
+    img: `https://app.mindclass.co.uk/static/media/counsellorImage.5f296e5ee6d9e5398903.png`,
+    imgAlt: `Carl Turner, MindClass Clinical Lead, CYPMHS BA (Hons), PG dip`
+}
 
 export function CounsellorMainBlock(){
     return(
         <Container fluid={true}>
             <Row>
                 <Col xs={12} xl={6}>
-                    <div dangerouslySetInnerHTML={{__html: blockHTML}} />
+                    <h2>{block.title}</h2>
+                    <div dangerouslySetInnerHTML={{__html: block.body}} />
                 </Col>
                 <Col xs={12} xl={6}>
                     <Image
-                        src={blockImageSrc}
-                        alt={blockImageAlt}
+                        src={block.img}
+                        alt={block.imgAlt}
                         width="1199"
                         height="800"
                         style={{width:'100%', height:'auto'}}
