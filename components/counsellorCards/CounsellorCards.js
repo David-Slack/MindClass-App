1d8e23ec9d47e013ee0b37376bb63c5a12ea7ee0
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-11 13:42:46 +0100

    Card #10: Changed the counsellor card styles

--- components/counsellorCards/CounsellorCards.js
+++ components/counsellorCards/CounsellorCards.js
@@ -1,35 +1,44 @@
-import { Card, CardGroup } from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 import styles from "./CounsellorCards.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
 export function CounsellorCards({ collection }) {
-    return (
-        <CardGroup className={styles.cardGroup}>
+    const counsellorsPerRow = 3;
+    const counsellorGroups = [];
+    let currentGroup = [];
 
-            {collection.map((counsellor) => (
+    collection.forEach((counsellor, index) => {
+        currentGroup.push(
+            <Card className={styles.card} bg={counsellor.colour} key={counsellor.id}>
+                <Card.Body>
+                    <Card.Title>{counsellor.name}</Card.Title>
+                    <Card.Text className={styles.cardText}>
+                        {counsellor.homepageText}
+                    </Card.Text>
+                </Card.Body>
+                <Image
+                    src={counsellor.image}
+                    alt={counsellor.name}
+                    className={styles.img}
+                    width={655}
+                    height={374}
+                    priority={collection.indexOf(counsellor) < 2} // Prioritize the first few
+                />
+                <span className={`${styles.arrow} cardArrow`}>&rarr;</span>
+                <Link href={`/counsellors/${counsellor.id}`} className="stretched-link"></Link>
+            </Card>
+        );
 
-                <Card className={styles.card} bg={counsellor.colour}>
-                    <Card.Body>
-                        <Card.Title>{counsellor.name}</Card.Title>
-                        <Card.Text className={styles.cardText}>
-                            {counsellor.homepageText}
-                        </Card.Text>
-                    </Card.Body>
-                    <Image
-                        src={counsellor.image}
-                        alt={counsellor.name}
-                        className={styles.img}
-                        width={655}
-                        height={374}
-                        priority={collection.indexOf(counsellor) < 2} // Prioritize the first few
-                    />
-                    <span className={`${styles.arrow} cardArrow`}>&rarr;</span>
-                    <Link href={`/counsellors/${counsellor.id}`} className="stretched-link"></Link>
-                </Card>
+        if ((index + 1) % counsellorsPerRow === 0 || index === collection.length - 1) {
+            counsellorGroups.push(
+                <Row md={3} key={`group-${index}`} className={styles.cardGroup}>
+                    {currentGroup}
+                </Row>
+            );
+            currentGroup = [];
+        }
+    });
 
-            ))}
-
-        </CardGroup>
-    );
+    return <>{counsellorGroups}</>;
 }
