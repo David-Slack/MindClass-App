95b49c82426db6bdac4544e007bdf7da908f0c92
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-22 15:56:27 +0100

    Card #47: Added video section

---
+++ pages/video.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import {TitleHeader} from "@/components/titleHeader/TitleHeader";
+import {MeditationsCards} from "@/components/cards/meditationsCards/MeditationsCards";
+import {Row} from "react-bootstrap";
+import {getVideoCollection} from "@/helpers/firebase/getVideoCollection";
+
+export async function getServerSideProps() {
+    return getVideoCollection("short"); // relax is the name of the meditation type
+}
+export default function Video({ collection }) {
+    const title = 'Video';
+    const description = "Take some time out to watch our latest videos";
+
+    return (
+        <>
+            <Head>
+                <title>{`${title} | MindClass`}</title>
+                <meta name="description" content={description} />
+            </Head>
+
+            <TitleHeader title={title} subtitle={description} />
+            <Row>
+                <MeditationsCards collection={collection} />
+            </Row>
+        </>
+    );
+}
