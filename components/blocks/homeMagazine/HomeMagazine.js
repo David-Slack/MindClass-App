400ce1488dbe71195b97db6d349810d1b30eb3e9
Dave Slack <davidkslack@yahoo.co.uk>
2025-04-30 15:21:56 +0100

    Card #43: Added magazine side scroller buttons

--- components/blocks/homeMagazine/HomeMagazine.js
+++ components/blocks/homeMagazine/HomeMagazine.js
@@ -2,8 +2,29 @@ import {Col, Row} from "react-bootstrap";
 import Link from "next/link";
 import {MagazineCards} from "@/components/cards/magazineCards/MagazineCards";
 import styles from "./HomeMagazine.module.css";
+import { useRef } from "react";
 
 export default function HomeMagazine({articles}) {
+    const scrollContainerRef = useRef(null);
+
+    const scrollLeft = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollBy({
+                left: -400,
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollBy({
+                left: 400,
+                behavior: 'smooth',
+            });
+        }
+    };
+
     return (
         <div className={styles.magazineBlock}>
             <Row>
@@ -15,8 +36,12 @@ export default function HomeMagazine({articles}) {
                     <p><Link href={'/magazine'}>View all</Link></p>
                 </Col>
             </Row>
-            <div className="card-group-scroll">
-                <MagazineCards magazineArticles={articles} simple={true} className="magazine-cards-flex-row" />
+            <div className="scrollable-container">
+                <button className="scroll-button" onClick={scrollLeft}>&lt;</button>
+                <div className="card-group-scroll" ref={scrollContainerRef}>
+                    <MagazineCards magazineArticles={articles} simple={true} />
+                </div>
+                <button className="scroll-button" onClick={scrollRight}>&gt;</button>
             </div>
         </div>
     );
